033dbcb92d10df0131b2b4059ccd62fd8c00a518
Christopher Brown <brown@opsys.cs.umsl.edu>
2021-09-07 02:48:20 -0500

    added more stuffff

--- main.cpp
+++ main.cpp
@@ -16,8 +16,44 @@ using namespace std;
 
 
 int main ( int argc, char **argv ){
+	
+	extern char *optarg;
+	
+	char fileName = "logfile.text";
+	int secs = 1;
+
+	while((op = gettopt( argc, argv, "ht:")) != -1 ){
+		switch(opt){
+			case 'h':
+				cout << "help message\n";
+				exit( 0 );
+			break;
+			
+			case 't':
+				secs = atoi(optarg);
+			break;
+
+			case '?':
+				cout << "Invalid option\n";
+				exit( 1 );
+			break;
+
+			default:
+				cout << "Invalid arguments\n";
+				exit( 1 );
+			break;
+
+		}
 
 
+
+
+
+
+
+
+
+	}
 
 	return 0;
 }
